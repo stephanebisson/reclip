dcfd612dc48305b546f5e00ddc9a4de32589025a
Stephane Bisson <sbisson@wikimedia.org>
2017-10-24 15:20:45 +0530

    make search case-insensitive; make refs to clippable

--- src/ui.jsx
+++ src/ui.jsx
@@ -29,6 +29,11 @@ const Item = ( { item } ) =>
 		<span>{item}</span>
 	</div>
 
+const Footer = () =>
+	<div>
+		<span>This is a footer</span>
+	</div>
+
 class InputSuggestion extends React.Component {
 	constructor( props ) {
 	    super( props )
@@ -37,13 +42,17 @@ class InputSuggestion extends React.Component {
 		this.filterEntries( '' )
 	}
 	componentDidMount() {
-		let rawNode = ReactDOM.findDOMNode(this)
+		this.scrollableContainerNode = this.props.scrollableContainerNode || window
+
+		console.log( this.clippableNode, this.clippableContainer, this.scrollableContainerNode )
 	}
 	handleChange( event ) {
 		this.filterEntries( event.target.value )
 	}
 	filterEntries( input ) {
-		let suggestions = input ? entries.filter( e => e.startsWith( input ) ) : entries
+		let suggestions = input ?
+			entries.filter( e => e.toLowerCase().startsWith( input.toLowerCase() ) ) :
+			entries
 		suggestions = suggestions.sort()
 		this.setState( { input, suggestions } )
 	}
@@ -55,8 +64,11 @@ class InputSuggestion extends React.Component {
 					placeholder="Type here..."
 					value={ this.state.input } 
 					onChange={ this.handleChange.bind( this ) } />
-				<div className="suggestions">
-					{this.state.suggestions.map( s => <Item key={s} item={s} /> )}
+				<div className="suggestions-box" ref={ e => this.clippableContainer = e }>
+					<div className="suggestions" ref={ e => this.clippableNode = e }>
+						{this.state.suggestions.map( s => <Item key={s} item={s} /> )}
+					</div>
+					<Footer />
 				</div>
 			</div>
 		)
